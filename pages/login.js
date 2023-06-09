a83b993a1e8cd44ae4c88e4df7575aef6ce7c86a
panditdhamdhere <dhamdherepandit@gmail.com>
2023-06-09 09:58:32 +0530

    or added

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {} from "react-icons/io"
+import { IoLogoGoogle, IoLogoFacebook } from "react-icons/io";
 
 const Login = () => {
   return (
     <div className="h-[100vh] flex justify-center items-center bg-c1">
-      <div className="flex items-center flex-col">
+      <div className="flex items-center flex-col w-[600px]">
         <div className="text-center">
           <div className="text-4xl font-bold">Login to your Account</div>
           <div className="mt-3 text-c3">
@@ -15,17 +15,23 @@ const Login = () => {
         <div className="flex items-center gap-2 w-full mt-10 mb-5">
           <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]">
             <div className="flex items-center justify-center gap-3 text-white font-semibold bg-c1 w-full h-full rounded-md">
+              <IoLogoGoogle size={24} />
               <span>Login with Google</span>
             </div>
           </div>
 
           <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]">
             <div className="flex items-center justify-center gap-3 text-white font-semibold bg-c1 w-full h-full rounded-md">
+              <IoLogoFacebook size={24} />
               <span>Login with Facebook</span>
             </div>
           </div>
+        </div>
 
-
+        <div className="flex items-center gap-1">
+          <span className="w-5 h-[1px] bg-c3"></span>
+          <span className="text-c3 font-semibold">OR</span>
+          <span className="w-5 h-[1px] bg-c3"></span>
         </div>
       </div>
     </div>
