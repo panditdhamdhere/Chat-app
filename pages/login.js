2ae65875321373fa0e6301abaf88dcdafa3f8424
panditdhamdhere <dhamdherepandit@gmail.com>
2023-06-10 09:04:36 +0530

    log in page completed

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 import { IoLogoGoogle, IoLogoFacebook } from "react-icons/io";
 
 const Login = () => {
   return (
     <div className="h-[100vh] flex justify-center items-center bg-c1">
-      <div className="flex items-center flex-col w-[600px]">
+      <div className="flex items-center flex-col">
         <div className="text-center">
           <div className="text-4xl font-bold">Login to your Account</div>
           <div className="mt-3 text-c3">
@@ -32,6 +33,36 @@ const Login = () => {
           <span className="w-5 h-[1px] bg-c3"></span>
           <span className="text-c3 font-semibold">OR</span>
           <span className="w-5 h-[1px] bg-c3"></span>
+        </div>
+
+        <form className="flex flex-col items-center gap-3 w-[500px] mt-5">
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3"
+            autoComplete="off"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3"
+            autoComplete="off"
+          />
+          <div className="text-right w-full text-c3">
+            <span className="cursor-pointer">Forget Password ?</span>
+          </div>
+          <button className="mt-4 w-full h-14 rounded-xl outline-none text-base font-semibold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">
+            Log in to your account
+          </button>
+        </form>
+        <div className="flex justify-center gap-1 text-c3 mt-5">
+          <span>Not a member yet ?</span>
+          <Link
+            href="/register"
+            className="font-semibold text-white underline underline-offset-2 cursor-pointer"
+          >
+            Register Now
+          </Link>
         </div>
       </div>
     </div>
